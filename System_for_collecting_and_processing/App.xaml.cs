2c831c85f2cd16f51208449da013d1a98f228d38
IRunnerI <98248133+IRunnerI@users.noreply.github.com>
2024-11-02 15:17:31 +0300

    Start project

---
+++ System_for_collecting_and_processing/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace System_for_collecting_and_processing
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
