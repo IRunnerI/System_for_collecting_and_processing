bfb1325737b6f9caf5ed56e0b20f650e0a9a167c
IRunnerI <artemmfedlol@mail.ru>
2024-12-02 23:41:33 +0300

    New Project

---
+++ Uod/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uod
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+            double[] dataX = { 0, 1, 2, 3, 4, 5 };
+            double[] dataY = { 0, 1, 4, 9, 16, 25 };
+            double[] dataX2 = {0, -1, -2, -3, -4, -5 };
+            double[] dataY2 = { 0, 1, 4, 9, 16, 25 };
+            WpfPlot1.Plot.Add.Scatter(dataX, dataY);
+            WpfPlot1.Plot.Add.Scatter(dataX2, dataY2);
+            WpfPlot1.Refresh();
+        }
+    }
+}
